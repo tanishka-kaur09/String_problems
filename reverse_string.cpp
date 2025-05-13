4ce8bd9c8d28df5af9bdeaa3300d273ab0a77e99
Tanishka Kaur <tanishkakaur89@gmail.com>
2025-05-13 22:27:23 +0530

    Add files via upload

---
+++ reverse_string.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string s="hello";
+    cout<<s<<endl;
+    int start=0;
+    // int end=s.size()-1;
+    
+    ind end=0;
+    while(s[end]!="\0")
+    end++;
+    end--;
+
+    while(start<=end){
+        swap(s[start],s[end]);
+        start++;
+        end--;
+    }
+
+    cout<<s;
+}

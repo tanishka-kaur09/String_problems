e678847146d503f7910aba2d25760102f439c4b2
Tanishka Kaur <tanishkakaur89@gmail.com>
2025-05-16 20:52:25 +0530

    Add files via upload

---
+++ check_if_array_strings_are_equal.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        int n=word1.size(),m=word2.size();
+        string s1,s2;
+        for(int i=0;i<n;i++)
+            s1+=word1[i];
+        for(int i=0;i<m;i++)
+            s2+=word2[i];
+
+        return s1==s2;
+}
+int main(){
+    vector<string> word1 = {"ab", "c"};
+    vector<string> word2 = {"a", "bc"};
+
+    if (arrayStringsAreEqual(word1, word2))
+        cout << "The arrays form the same string." << endl;
+    else
+        cout << "The arrays do not form the same string." << endl;
+
+    return 0;
+
+}
